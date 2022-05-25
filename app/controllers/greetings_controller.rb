620e4dddf64f6b7ebf2fd04ea8bd805eb94e49c5
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-25 13:08:11 -0500

    Add Greeting model, controllers & routes

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,12 @@
+class GreetingsController < ApplicationController
+
+    def index
+        @greetings = Greeting.all
+        render json: @greetings
+    end
+
+    def random
+        @greeting = Greeting.where(id: rand(1..5))
+        render json: @greeting
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :greetings, only: [:index]
+  get '/greetings/random', to: 'greetings#random'
 
   # Defines the root path route ("/")
   # root "articles#index"
