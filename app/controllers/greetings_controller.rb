baf31d97c1ca1de2978842987cf68d66b9d6a7da
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-25 13:29:35 -0500

    Add linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,12 +1,11 @@
 class GreetingsController < ApplicationController
+  def index
+    @greetings = Greeting.all
+    render json: @greetings
+  end
 
-    def index
-        @greetings = Greeting.all
-        render json: @greetings
-    end
-
-    def random
-        @greeting = Greeting.where(id: rand(1..5))
-        render json: @greeting
-    end
+  def random
+    @greeting = Greeting.where(id: rand(1..5))
+    render json: @greeting
+  end
 end
