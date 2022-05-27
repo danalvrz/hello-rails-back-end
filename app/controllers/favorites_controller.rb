09833d25006d5642f01d6521f7d2c7c3d8645f45
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-26 19:50:26 -0500

    Add CRUD methods to main models

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :authorize_request_fav, except: :create
-  before_action :find_user, except: %i[create index]
+  before_action :authorize_request_fav
 
   # GET /users/:user_id/favorites
   def index
@@ -9,59 +8,27 @@ class FavoritesController < ApplicationController
     render json: @favorites
   end
 
-  # # GET /favorites/1.json
-  # def show
-  #   @favorite = Favorite.find(params[:id])
-  #   render json: @favorite
-  # end
-
-  # # GET /favorites/new
-  # def new
-  # end
-
-  # # GET /favorites/1/edit
-  # def edit
-  # end
-
-  # # POST /favorites or /favorites.json
-  # def create
-  #   @favorite = Favorite.create(
-  #     user_id: params[:user_id],
-  #     car_id: params[:car_id]
-  #   )
-  #   render json: @favorite
-  # end
-
-  # # PATCH/PUT /favorites/1 or /favorites/1.json
-  # def update
-  #   @favorite = Favorite.find(params[:id])
-  #   @favorite.update(
-  #     user_id: params[:user_id],
-  #     car_id: params[:car_id]
-  #   )
-  #   render json: @favorite
-  # end
-
-  # def update_favorite
-  # end
-
-  # # DELETE /favorites/1 or /favorites/1.json
-  # def destroy
-  #   @favorites = Favorite.all
-  #   @favorite = Favorite.find(params[:id])
-  #   @favorite.destroy
-  #   render json: @favorites
-  # end
-
-  private
-
-  # def find_user
-  #   @user = User.find(params[:user_id])
-  #   rescue ActiveRecord::RecordNotFound
-  #     render json: { errors: 'User not found' }, status: :not_found
-  # end
-
-  # Only allow a list of trusted parameters through.
-  def favorite_params
+  # POST users/:user_id/favorites or users/:user_id/favorites.json
+  # body { greeting_id: "id"}
+  def create
+    @user = User.find(params[:user_id])
+    @favorite = Favorite.create(user_id: @user.id, greeting_id: params[:greeting_id])
+    if @favorite.valid?
+      render json: { message: "Favorite created!"}
+    else
+      render json: {status: 500, message: @greeting.id, errors: @favorite.errors.full_messages}
+    end
   end
+
+  # DELETE users/:user_id/favorites/:id or users/:user_id/favorites/:id.json
+  def destroy
+    @user = User.find(params[:user_id])
+    @favorites = Favorite.includes(:user).where(user_id: @user.id).order(created_at: :desc)
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    render json: {message: "favorite deleted!"}
+  end
+
+  #private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :greetings, only: [:index]
   resources :users do
-    resources :favorites, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :favorites, only: [:index, :create, :destroy]
   end
   get '/greetings/random', to: 'greetings#random'
   post "/login", to: "auth#login"
